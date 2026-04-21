e10d195e8612c1036f18ab55679c45daade81dab
windy <707298413@qq.com>
2026-04-22 03:55:19 +0800

    init

--- build.zig
+++ build.zig
@@ -5,13 +5,13 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const root_module = b.createModule(.{
-        .root_source_file = b.path("src/cli/main.zig"),
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
     const exe = b.addExecutable(.{
-        .name = "kite-cli",
+        .name = "kite",
         .root_module = root_module,
     });
 
